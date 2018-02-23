2580584736793d9e14f25e8e5d8c8175cdc1426e
Ajaypal Cheema <ajaypal.cheema@sgainc.com>
2018-02-23 14:02:13 -0500

    factorial recursion

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -3,3 +3,22 @@ Implement factorial.
 
 factorial(5) => 5*4*3*2*1 => 120
 */
+let factorial = (n) => {
+    if (n === 1) {
+        return 1;
+    }
+    return n*factorial(n-1);
+};
+factorial(5);
+
+/**
+Non-recursive:
+let factorial = (n) => {
+    let fact = 1;
+    for(let i = 1; i <= n; i += 1){
+        fact *= i;
+    }
+    return fact;
+};
+factorial(5);
+*/
