9a294d5a78f477e88651d63ed3194b7124205c71
Ajaypal Cheema <ajaypal.cheema@sgainc.com>
2018-02-23 14:30:11 -0500

    fibonacci

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -8,3 +8,27 @@ output   0 1 1 2 3 5 8 13 21 34 ...
 
 What is the time complexity? Can you think of optimizing your solution? (Hint: look up dynamic programming)
 */
+let fibo = (n) => {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    }
+    return fibo(n-1) + fibo(n-2);
+};
+fibo(9);
+
+/**
+Non-recursive solution
+let fibo = (n) => {
+    let arr = [0, 1];
+    if(n <= 2) {
+        return 1;
+    }
+    for (let i = 2; i <= n; i += 1) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[n];
+};
+fibo(9);
+*/
