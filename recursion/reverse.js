e2e0e6b4706ea45fb6ed19eaa552bf323cdbc340
Ajaypal Cheema <ajaypal.cheema@sgainc.com>
2018-03-01 16:40:05 -0500

    reverse string recursive

--- recursion/reverse.js
+++ recursion/reverse.js
@@ -4,3 +4,19 @@ Implement a function that will reverse a string recursively.
 reverse('abcdefg')
 => 'gfedcba'
 */
+function reverseStr(str) {
+    let rev = [];
+    let arr = str.split('');
+    function reverser(arr) {
+        if (arr.length === 0) {
+            return;
+        } else {
+            rev.push(arr.pop());
+        }
+        return reverser(arr);
+    };
+    reverser(arr);
+    return rev.join('');
+};
+
+reverseStr('abcdefg');
