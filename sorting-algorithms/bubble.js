3c2e0d107cb7e7e180bb8a4f8a3dd94badd81ce3
Ajaypal Cheema <ajaypal.cheema@sgainc.com>
2018-03-04 14:05:39 -0500

    bubble sort

--- sorting-algorithms/bubble.js
+++ sorting-algorithms/bubble.js
@@ -17,5 +17,22 @@ Optimizations:
 Variants:
 - Implement cocktail sort (for each pass find both min and max values and sort in both directions). How does this impact performance?
 (https://en.wikipedia.org/wiki/Cocktail_sort)
-
 */
+
+// O(n^2)
+function bubbleSort(list) {
+    // Outler loop is O(n)
+    for (let i = 0; i< list.length; i += 1) {
+        // inner loop is O(n)
+        for (let j = 1; j <= list.length-1; j += 1) {
+            if (list[j-1] > list[j]) { // constant
+                let temp = list[j]; // constant
+                list[j] = list[j-1]; // constant
+                list[j-1] = temp; // constant
+            }
+        }
+    }
+    return list; // constant
+}
+
+bubbleSort([9,8,1,3,5,6,7,11,0,4]); // [0, 1, 3, 4, 5, 6, 7, 8, 9, 11]
