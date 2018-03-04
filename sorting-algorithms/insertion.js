5c8d08575b5af01caf2c3a2d4ef884ed43294684
Ajaypal Cheema <ajaypal.cheema@sgainc.com>
2018-03-04 16:47:41 -0500

    insertionSort

--- sorting-algorithms/insertion.js
+++ sorting-algorithms/insertion.js
@@ -29,3 +29,20 @@ now repeat for next unsorted element
 (https://en.wikipedia.org/wiki/Shellsort)
 
 */
+// O(n^2)
+function insertionSort(arr) {
+    let i = 1;
+    while (i < arr.length) {
+        let j = i;
+        while (j > 0 && arr[j-1] > arr[j]) {
+            let temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+            j -= 1;
+        }
+        i += 1;
+    }
+    return arr;
+}
+
+insertionSort([9,8,1,3,5,6,7,11,0,4]); // [ 0, 1, 3, 4, 5, 6, 7, 8, 9, 11 ]
